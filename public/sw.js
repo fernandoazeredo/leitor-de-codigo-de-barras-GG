5ac99bf9f504c4181de87ff24352691011740b52
fernandoazeredo <132702186+fernandoazeredo@users.noreply.github.com>
2026-09-09 16:16:45 -0300

    Atualiza cache PWA para nova interface de login

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,5 @@
-const CACHE='leitor-gg-v1';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/manifest.webmanifest','/icon.svg']))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+const CACHE='leitor-gg-v2';
+const ANTIGOS=['leitor-gg-v1'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/manifest.webmanifest','/icon.svg'])))});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all(ANTIGOS.map(c=>caches.delete(c))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request).then(r=>r||caches.match('/'))));});
