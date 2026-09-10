fc22c3eb64af13ca68c054344e1d04c257eac739
fernandoazeredo <132702186+fernandoazeredo@users.noreply.github.com>
2026-09-10 07:51:11 -0300

    Carrega estilos do menu Dicas e tema

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import AuthGate from './AuthGate';
 import { HeaderTools } from './HeaderTools';
 import './styles.css';
+import './header-tools.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
