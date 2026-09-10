7b6d65aa9aee1e7e8cb5bce31ffa7b0b2cf85511
fernandoazeredo <132702186+fernandoazeredo@users.noreply.github.com>
2026-09-10 07:50:45 -0300

    Integra Dicas e seletor de tema no app

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AuthGate from './AuthGate';
+import { HeaderTools } from './HeaderTools';
 import './styles.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><AuthGate/></React.StrictMode>);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <AuthGate/>
+    <HeaderTools/>
+  </React.StrictMode>
+);
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => navigator.serviceWorker.register('/sw.js').catch(() => undefined));
